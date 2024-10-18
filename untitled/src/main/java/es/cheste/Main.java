1927a9226ead117af05c923f54e24accfa08496b
diego <dieferrui@alu.edu.gva.es>
2024-10-18 13:38:47 +0200

    Databases II

--- untitled/src/main/java/es/cheste/Main.java
+++ untitled/src/main/java/es/cheste/Main.java
@@ -12,25 +12,45 @@ public class Main {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String server = "localhost";
-        int port = 3306;
-        String user = "root";
-        String pwd = "root";
-        String DBName = "Empresa";
-        String urlConnection = "jdbc:mysql://" + server + ":" + port;
-        urlConnection += "/" + DBName;
-        urlConnection += "?useUnicode=true&characterEncoding=UTF-8";
+        ConexionBD c = new ConexionBD();
 
-        Connection c = DriverManager.getConnection(urlConnection, user, pwd);
+        // generarTablas(c);
+        // preparedStatement(c);
+        preparedStatement2(c);
+        c.desconectar();
 
     }
 
-    private static void preparedStatement(Connection c) {
-        try {
-            PreparedStatement sInsert = c.prepareStatement("INSERT INTO CLIENTES1(DNI,APELLIDOS,CP) VALUES(?,?,?)");
+    private static void generarTablas(ConexionBD c) {
+        try(Statement statement = c.getConnection().createStatement()) {
+            statement.execute("CREATE TABLE CLIENTES (DNI CHAR(9)NOT NULL , APELLIDOS VARCHAR(32) NOT NULL, CP CHAR(5), PRIMARY KEY (DNI))");
+            statement.execute("CREATE TABLE FACTURAS (\n" +
+                    "    NUM_FACTURA INT AUTO_INCREMENT,\n" +
+                    "    DNI_CLIENTE CHAR(9) NOT NULL,\n" +
+                    "    PRIMARY KEY (NUM_FACTURA),\n" +
+                    "    CONSTRAINT FK_FACT_DNI_CLIENTES FOREIGN KEY (DNI_CLIENTE)\n" +
+                    "        REFERENCES CLIENTES (DNI)\n" +
+                    ");\n");
+            statement.execute("CREATE TABLE LINEAS_FACTURA (" +
+                    "NUM_FACTURA INTEGER NOT  NULL, " +
+                    "LINEA_FACTURA SMALLINT NOT NULL, " +
+                    "CONCEPTO VARCHAR(32) NOT NULL, " +
+                    "CANTIDAD SMALLINT NOT NULL, " +
+                    "PRIMARY KEY (NUM_FACTURA, LINEA_FACTURA), " +
+                    "FOREIGN KEY FK_LINEAFACT_NUM_FACTURA (NUM_FACTURA)" +
+                    "REFERENCES FACTURAS (NUM_FACTURA))");
 
-            c.setAutoCommit(false);
+        } catch (SQLException e){
+            LOGGER.error(e.getMessage());
+
+        }
+    }
+
+    private static void preparedStatement(ConexionBD c) {
+        try {
+            PreparedStatement sInsert = c.getConnection().prepareStatement("INSERT INTO CLIENTES(DNI,APELLIDOS,CP) VALUES(?,?,?)");
+
+            c.getConnection().setAutoCommit(false);
 
             int i = 1;
             sInsert.setString(i++, "78901234X");
@@ -50,17 +70,70 @@ public class Main {
             sInsert.setInt(i, 29730);
             sInsert.executeUpdate();
 
-            c.commit();
+            c.getConnection().commit();
 
         } catch (SQLException e) {
             LOGGER.error("Error al insertar en la base de datos.");
 
             try {
-                c.rollback();
+                c.getConnection().rollback();
                 LOGGER.error("Se hace ROLLBACK");
 
             } catch (SQLException err) {
                 LOGGER.error("Error haciendo ROLLBACK");
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+
+        }
+    }
+
+    private static void preparedStatement2 (ConexionBD c) {
+        try (PreparedStatement sInsertFact = c.getConnection().prepareStatement("INSERT INTO FACTURAS (DNI_CLIENTE) VALUES (?)",
+                Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement sInsertLineaFact = c.getConnection().prepareStatement(
+                    "INSERT INTO LINEAS_FACTURA (NUM_FACTURA, LINEA_FACTURA, CONCEPTO, CANTIDAD)" +
+                            "VALUES (?, ?, ?, ?)")) {
+            c.getConnection().setAutoCommit(false);
+
+            int i = 1;
+            sInsertFact.setString(i++, "78901234X");
+            sInsertFact.executeUpdate();
+
+            ResultSet rs = sInsertLineaFact.getGeneratedKeys();
+            rs.next();
+            int numFact = rs.getInt(1);
+
+            int lineaFact = 1;
+            i = 1;
+            sInsertLineaFact.setInt(i++, numFact);
+            sInsertLineaFact.setInt(i++, lineaFact++);
+            sInsertLineaFact.setString(i++, "TUERCAS");
+            sInsertLineaFact.setInt(i++, 25);
+            sInsertLineaFact.executeUpdate();
+
+            i = 1;
+            sInsertLineaFact.setInt(i++, numFact);
+            sInsertLineaFact.setInt(i++, lineaFact);
+            sInsertLineaFact.setString(i++, "TORNILLOS");
+            sInsertLineaFact.setInt(i++, 250);
+            sInsertLineaFact.executeUpdate();
+
+            c.getConnection().commit();
+
+        } catch (SQLException e) {
+            LOGGER.error("Error al insertar en la base de datos.");
+            e.printStackTrace();
+
+            try {
+                c.getConnection().rollback();
+                LOGGER.error("Se hace ROLLBACK");
+
+            } catch (SQLException err) {
+                LOGGER.error("Error haciendo ROLLBACK");
+                e.printStackTrace();
 
             }
 
