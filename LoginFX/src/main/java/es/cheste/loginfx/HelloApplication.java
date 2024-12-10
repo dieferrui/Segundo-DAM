ba541642ad5e9d87ec29ad633b5ef4efea9cf853
diego <dieferrui@alu.edu.gva.es>
2024-12-10 08:49:31 +0100

    Amend LoginFX II

--- LoginFX/src/main/java/es/cheste/loginfx/HelloApplication.java
+++ LoginFX/src/main/java/es/cheste/loginfx/HelloApplication.java
@@ -2,19 +2,56 @@ package es.cheste.loginfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.stage.*;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+
+        scene.setOnMousePressed(event -> {
+            stage.setUserData(new double[] { event.getScreenX(), event.getScreenY() });
+        });
+
+        scene.setOnMouseDragged(event -> {
+            double[] offset = (double[]) stage.getUserData();
+            double deltaX = event.getScreenX() - offset[0];
+            double deltaY = event.getScreenY() - offset[1];
+            stage.setX(stage.getX() + deltaX);
+            stage.setY(stage.getY() + deltaY);
+
+            stage.setUserData(new double[] { event.getScreenX(), event.getScreenY() });
+        });
+
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
+
+        stage.setWidth(1024);
+        stage.setHeight(768);
+
         stage.show();
+    }
+
+    private void handleMousePressed(MouseEvent event) {
+        xOffset = event.getScreenX() - event.getSceneX();
+        yOffset = event.getScreenY() - event.getSceneY();
+    }
+
+    private void handleMouseDragged(MouseEvent event) {
+        Stage stage = (Stage) event.getSource();
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset);
     }
 
     public static void main(String[] args) {
--- LoginFX/src/main/java/es/cheste/loginfx/HelloController.java
+++ LoginFX/src/main/java/es/cheste/loginfx/HelloController.java
@@ -1,14 +1,24 @@
 package es.cheste.loginfx;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.Optional;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField usuarioField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void onCloseButtonClicked() {
+        Platform.exit();
     }
 }
