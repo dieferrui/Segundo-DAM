8b889497dd631c2d37eb9cb910766dbe782a7539
diego <dieferrui@alu.edu.gva.es>
2025-02-10 14:29:19 +0100

    RestTest3

--- RestExample/src/main/java/com/openwebinars/rest/configuration/MiConfiguracion.java
+++ RestExample/src/main/java/com/openwebinars/rest/configuration/MiConfiguracion.java
@@ -3,11 +3,29 @@ package com.openwebinars.rest.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MiConfiguracion {
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // registry.addMapping("/**");
+
+                // Ejemplo
+                registry.addMapping("/producto/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+            }
+        };
     }
 }
